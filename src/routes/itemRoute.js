3ce2872acd2d08267eb39bf300c52d2424be6227
Sweeney Ngo <sweeneyngo@gmail.com>
2021-11-11 13:35:22 -0800

    Added heroku to whitelist

--- src/routes/itemRoute.js
+++ src/routes/itemRoute.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import models from "../models";
 const router = Router();
 
-//
+// [GET] Retrieve all items
 router.get("/", async (req, res, next) => {
   const items = await models.Item.find().catch(next);
   console.log(items);
